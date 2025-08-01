fb163b0758403acd19855f324ddd7462ddbea0b5
Archita-Rai <raiarchita98@gmail.com>
2025-08-01 22:35:56 +0530

    connection route functionality is completed

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,17 +1,18 @@
 import User from "../models/user.model.js";
 import Profile from "../models/profile.model.js";
+import ConnectionRequest from "../models/connection.model.js";
 import bcrypt from "bcrypt";
 import crypto from "crypto";
 import PDFDocument from "pdfkit";
 import fs from "fs";
+import { connect } from "http2";
 
 //pdf creator function
-const convertUserDataToPDF = async(userData) => {
+const convertUserDataToPDF = async (userData) => {
   const doc = new PDFDocument();
   const outputPath = crypto.randomBytes(32).toString("hex") + ".pdf";
   const stream = fs.createWriteStream("uploads/" + outputPath);
   doc.pipe(stream);
-  console.log(userData.userId.profilePicture)
   doc.image(`uploads/${userData.userId.profilePicture}`, {
     align: "center",
     width: 100,
@@ -27,8 +28,8 @@ const convertUserDataToPDF = async(userData) => {
     doc.fontSize(14).text(`Position: ${work.position}`);
     doc.fontSize(14).text(`Years: ${work.years}`);
   });
-  doc.end()
-  
+  doc.end();
+
   return outputPath;
 };
 
@@ -38,7 +39,7 @@ export const register = async (req, res) => {
     const { name, email, password, username } = req.body;
     if (!name || !email || !password || !username) {
       console.log(`${name}  ${email}  ${password}  ${username}`);
-      return res.status(400).json({ massage: "All fields are required" });
+      return res.status(400).json({ message: "All fields are required" });
     }
     const user = await User.findOne({
       email,
@@ -61,7 +62,7 @@ export const register = async (req, res) => {
     await profile.save();
     return res.json({ message: "User has been created" });
   } catch (error) {
-    return res.status(500).json({ massage: error.massage });
+    return res.status(500).json({ message: error.message });
   }
 };
 
@@ -71,7 +72,7 @@ export const login = async (req, res) => {
     const { password, email } = req.body;
     if (!email || !password) {
       console.log(` ${email}  ${password}`);
-      return res.status(400).json({ massage: "All fields are required" });
+      return res.status(400).json({ message: "All fields are required" });
     }
     const user = await User.findOne({
       email,
@@ -81,13 +82,13 @@ export const login = async (req, res) => {
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
-      return res.status(400).json({ massage: "Invalid credential" });
+      return res.status(400).json({ message: "Invalid credential" });
 
     const token = crypto.randomBytes(32).toString("hex");
     await User.updateOne({ _id: user._id }, { token });
     return res.json({ token });
   } catch (error) {
-    return res.status(500).json({ massage: error.massage });
+    return res.status(500).json({ message: error.message });
   }
 };
 
@@ -179,7 +180,7 @@ export const getAllUserProfile = async (req, res) => {
   try {
     const profiles = await Profile.find().populate(
       "userId",
-      "name email username profilepicture"
+      "name email username profilePicture"
     );
 
     return res.json({ profiles });
@@ -195,7 +196,111 @@ export const dowloadUserResume = async (req, res) => {
     "userId",
     "name username email  profilePicture"
   );
-  console.log()
+  console.log();
   let outputPath = await convertUserDataToPDF(userProfile);
-  return res.json({"message":outputPath});
+  return res.json({ message: outputPath });
+};
+
+//to send connection request
+export const sendConnectionRequest = async (req, res) => {
+  const { token, connectionId } = req.body;
+  try {
+    const user = await User.findOne({ token });
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+
+    const connectionUser = await User.findOne({ _id: connectionId });
+
+    if (!connectionUser) {
+      return res.status(404).json({ message: "Connection user not found" });
+    }
+
+    const existingConnection = await ConnectionRequest.findOne({
+      userId: user._id,
+      connectionId: connectionUser._id,
+    });
+
+    if (existingConnection) {
+      return res.status(404).json({ message: "Request alraedy sent" });
+    }
+
+    const request = new ConnectionRequest({
+      userId: user._id,
+      connectionId: connectionUser._id,
+    });
+
+    await request.save();
+    return res.json("Request sent");
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// to see all my connection that I have send
+export const getAllMyConnectionRequest = async (req, res) => {
+  const { token } = req.body;
+  try {
+    const user = await User.findOne({ token });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const connection = await ConnectionRequest.find({
+      userId: user._id,
+    }).populate("connectionId", "name username email profilePicture");
+    return res.json(connection);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// to see all my connection request that is send by another people
+export const whatAreMyConnection = async (req, res) => {
+  const { token } = req.body;
+
+  try {
+    const user = await User.findOne({ token });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const connection = await ConnectionRequest.find({
+      connectionId: user._id,
+    }).populate("connectionId", "name username email profilePicture");
+    return res.json(connection);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// to accept and reject connection request
+export const acceptConnetionRequest = async (req, res) => {
+  const { token, requestId, action_type } = req.body;
+
+  try {
+    const user = await User.findOne({ token });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const connection = await ConnectionRequest.findOne({ _id: requestId });
+
+    if (!connection) {
+      return res.status(404).json({ message: "Connection not found" });
+    }
+
+    if (action_type == "accept") {
+      connection.status_accepted = true;
+    } else {
+      connection.status_accepted = false;
+    }
+    await connection.save();
+
+    return res.json({message:"Request accepted"})
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllUserProfile, getUserAndProfile, login, register,dowloadUserResume,updateProfileData,updateUserProfile,uploadProfilePicture } from "../controllers/user.controller.js";
+import { getAllUserProfile, getUserAndProfile, login, register,dowloadUserResume,updateProfileData,updateUserProfile,uploadProfilePicture, sendConnectionRequest, acceptConnetionRequest, getAllMyConnectionRequest, whatAreMyConnection } from "../controllers/user.controller.js";
 import multer from "multer";
 
 const router = Router();
@@ -24,7 +24,11 @@ router.route("/login").post(login);
 router.route("/user_update").post(updateUserProfile)
 router.route("/get_user_and_profile").get(getUserAndProfile)
 router.route("/update_profile_data").post(updateProfileData)
-router.route("/get_all_users").get(getAllUserProfile)
+router.route("/user/get_all_users").get(getAllUserProfile)
 router.route("/user/dowload_resume").get(dowloadUserResume)
+router.route("/user/send_connection_request").post(sendConnectionRequest);
+router.route("/user/getConnectionRequests").get(getAllMyConnectionRequest)
+router.route("/user/user_connection_requset").get(whatAreMyConnection)
+router.route("/user/accept_connection_request").post(acceptConnetionRequest)
 
 export default router;
