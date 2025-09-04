cb05b02b812cffbb7015d7b0ae2fb68a93c31001
Archita-Rai <raiarchita98@gmail.com>
2025-09-04 21:37:35 +0530

    adding url

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const { default: axios } = require("axios");
 
-export const BASE_URL = "http://localhost:9000"
+export const BASE_URL = "https://linkedin-clone-ctia.onrender.com"
 export const clientServer = axios.create({
   baseURL: BASE_URL,
 
